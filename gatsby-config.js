e31a6a3cec7ae3933fa723d94c35771bff2abc69
Akari Oomori <oomori.akari7766@gmail.com>
2021-09-14 09:08:56 +0900

    add:gatsby-plugin-eslint-v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,17 @@ module.exports = {
     /* Your site config here */
     plugins: [
         {
+            resolve: 'gatsby-plugin-eslint-v2',
+            options: {
+                test: /\.js$|\.jsx$/,
+                exclude: /(node_modules|cache|public)/,
+                options: {
+                    emitWarning: true,
+                    failOnError: false,
+                },
+            },
+        },
+        {
             resolve: `gatsby-plugin-typescript`,
             options: {
                 isTSX: true,
